65db307ab58c058b3d8d0eac3b8dfa71c3a4ac82
XateAorora <GamerXD8@yahoo.com>
2018-12-06 23:50:30 -0800

    Update mush.c

--- asgn6-Arjun/mush.c
+++ asgn6-Arjun/mush.c
@@ -11,7 +11,9 @@
 int main(int argc, char *argv[]){
     int terminal = dup(1);
     int keyboard = dup(0);
-    printf("8-D ");
+    if (isatty(fileno(stdin)) || isatty(fileno(stdout))){
+	    printf("8-D ");
+    }
     char orig[INPUTLIMIT + 1] = {'\0'};
     fgets(orig, INPUTLIMIT + 2, stdin);
     if(!strcmp(orig, "end\n")){
@@ -32,7 +34,11 @@ int main(int argc, char *argv[]){
 		    perror("Pipe Fails\n");
 		}
 	    }
-	
+	/*sigproc mask will happen right before fork*/
+	sigset_t x;
+        sigemptyset (&x);
+        sigaddset(&x, SIGINT);
+        sigprocmask(SIG_BLOCK, &x, NULL)
 	    if((childAddress = fork()) == 0){
 		if(i){
 		    printf("reading back\n");
@@ -53,6 +59,7 @@ int main(int argc, char *argv[]){
 		    output = open(stages[i]->output, O_RDWR | O_TRUNC | O_CREAT, 0777);
 		    dup2(output, STDOUT_FILENO);
 		}
+	   	sigprocmask(SIG_UNBLOCK, &x, NULL)
 		execvp(stages[i]->argumentVar[0], stages[i]->argumentVar);
 	    }else{
 		int check;
@@ -78,4 +85,31 @@ int main(int argc, char *argv[]){
 	fgets(orig, INPUTLIMIT + 2, stdin);
     }while(strcmp(orig, "end\n"));
     return 1;
+}
+
+int cd(char *pth){
+    char path[INPUTLIMIT];
+    char *NewLine = strchr(pth, '\n');
+    if(NewLine) {
+        *NewLine = '\0';
+    }
+    strcpy(path,pth);
+    char cwd[INPUTLIMIT];
+    if(pth[0] != '/'){ /*directory in cwd*/
+        getcwd(cwd,sizeof(cwd));
+        strcat(cwd,"/");
+        strcat(cwd,path);
+        int a = chdir(cwd);
+        if (a == -1){
+            return 1;
+        }
+    }
+    else{
+        int b=chdir(pth);
+        if (b == -1){
+            perror("chdir");
+            return 1;
+        }
+    }
+    return 0;
 }
